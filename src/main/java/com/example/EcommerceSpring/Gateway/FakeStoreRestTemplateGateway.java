87be1ca02933f811e2158c9093b957acbf274c06
Deepak singh <studydipu@gmail.com>
2026-03-04 16:56:03 +0530

    Added a Mapper Package and added a Categories Mapper

--- src/main/java/com/example/EcommerceSpring/Gateway/FakeStoreRestTemplateGateway.java
+++ src/main/java/com/example/EcommerceSpring/Gateway/FakeStoreRestTemplateGateway.java
@@ -1,30 +1,48 @@
 package com.example.EcommerceSpring.Gateway;
 
 import com.example.EcommerceSpring.Dtos.CategoryDTO;
+import com.example.EcommerceSpring.Dtos.FakeStoreCategoryResponseDTO;
+import com.example.EcommerceSpring.Dtos.RatingDTO;
+import com.example.EcommerceSpring.mapppers.GetAllCategoriesMapper;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RequestCallback;
+import org.springframework.web.client.ResponseExtractor;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
+
 
 import java.io.IOException;
 import java.util.List;
 @RequiredArgsConstructor
 @Component("FakeStoreRestTemplateGateway")
 public class FakeStoreRestTemplateGateway implements ICategoryGateway{
-    private  final RestTemplate restTemplate;
+    private final RestTemplateBuilder restTemplateBuilder;
     @Override
     public List<CategoryDTO> getAllCategories() throws IOException {
-        String url="https://fakestoreapi.com/products/";
-        ResponseEntity<List<CategoryDTO>>responses=
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        String url = "https://fakestoreapi.com/products/";
+
+        ResponseEntity<List<FakeStoreCategoryResponseDTO>> response =
                 restTemplate.exchange(
                         url,
                         HttpMethod.GET,
                         null,
-                        new ParameterizedTypeReference<List<CategoryDTO>>() {
-                        }
+                        new ParameterizedTypeReference<List<FakeStoreCategoryResponseDTO>>() {}
                 );
-        return responses.getBody();
+
+        if (response.getBody() == null) {
+            throw new IOException("Failed to fetch categories from FakeStore Api");
+        }
+        return GetAllCategoriesMapper.categoryDTOS(response.getBody());
+        }
     }
-}
---
+++ src/main/java/com/example/EcommerceSpring/mapppers/GetAllCategoriesMapper.java
@@ -0,0 +1,29 @@
+package com.example.EcommerceSpring.mapppers;
+
+import com.example.EcommerceSpring.Dtos.CategoryDTO;
+import com.example.EcommerceSpring.Dtos.FakeStoreCategoryResponseDTO;
+import com.example.EcommerceSpring.Dtos.FakeStoreProductResponseDTO;
+import com.example.EcommerceSpring.Dtos.RatingDTO;
+
+import java.util.List;
+
+public class GetAllCategoriesMapper {
+    public static FakeStoreCategoryResponseDTO fakeStoreCategoryResponseDTO(){
+        return null;
+    }
+    public static List<CategoryDTO> categoryDTOS(List<FakeStoreCategoryResponseDTO> dtos){
+        return dtos.stream().map(dto->CategoryDTO.builder().
+                id(dto.getId())
+                .title(dto.getTitle())
+                .price(dto.getPrice())
+                .description(dto.getDescription())
+                .category(dto.getCategory())
+                .image(dto.getImage())
+                .rating(
+                        RatingDTO.builder()
+                                .rate(dto.getRating().getRate())
+                                .count(dto.getRating().getCount())
+                        .build()).build()
+        ).toList();
+    }
+}
